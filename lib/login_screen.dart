a1f51f532462ba01a664adc7dd8d2607f670c113
CJ Fox <55763324+cj-fox@users.noreply.github.com>
2022-02-03 17:05:55 -0500

    Login screen allows user to sign in with their email and password.

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'donor_dashboard.dart';
 
 class LoginScreen extends StatefulWidget {
   static const id = 'login_screen';
@@ -9,6 +11,15 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  final _auth = FirebaseAuth.instance;
+
+  final _formKey = GlobalKey<FormState>();
+  
+  static final emailRegExp = RegExp(r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$');
+
+  String email="";
+  String password="";
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,6 +31,93 @@ class _LoginScreenState extends State<LoginScreen> {
           },
         ),
         title: Text('Login'),
+      ),
+      body: Form(
+        key: _formKey,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextFormField(
+                onChanged: (value){
+                  email = value;
+                },
+                validator: (value){
+                  if(value!.isEmpty){
+                    return "Please enter your email.";
+                  }
+                  else if(!emailRegExp.hasMatch(value)){
+                    return "Please enter a valid email address.";
+                  }
+                  else{
+                    return null;
+                  }
+                },
+                keyboardType: TextInputType.emailAddress,
+                textAlign: TextAlign.center,
+                decoration: const InputDecoration(
+                    hintText: "Email",
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(32.0)),
+                    )
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextFormField(
+                onChanged: (value){
+                  password = value;
+                },
+                validator: (value){
+                  if(value!.isEmpty || value.length < 6){
+                    return "Password must be at least 6 characters.";
+                  }
+                  else {
+                    return null;
+                  }
+                },
+                obscureText: true,
+                textAlign: TextAlign.center,
+                decoration: const InputDecoration(
+                    hintText: "Password",
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(32.0)),
+                    )
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 25.0, horizontal: 5.0),
+              child: Material(
+                elevation: 5.0,
+                color: Colors.blue,
+                borderRadius: BorderRadius.circular(32.0),
+                child: MaterialButton(
+                  child: const Text(
+                    'Login',
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                  ),
+                  onPressed: () async{
+                    if(_formKey.currentState!.validate()) {
+                      try {
+                        final user = await _auth.signInWithEmailAndPassword(email: email, password: password);
+                        Navigator.pushNamed(context, DonorDashboard.id);
+                      }
+                      catch (e) {
+                        print(e);
+                      }
+                    }
+                  },
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
