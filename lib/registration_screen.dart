8644823a40e1088b39e64c98e9085d6cf7dfd44e
CJ Fox <55763324+cj-fox@users.noreply.github.com>
2022-02-03 17:06:57 -0500

    Registration screen gives user option to register as either a donor or organization.

--- lib/registration_screen.dart
+++ lib/registration_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'donor_registration_screen.dart';
+import 'organization_registration_screen.dart';
 
 class RegistrationScreen extends StatefulWidget {
   static const id = 'registration_screen';
@@ -10,18 +12,73 @@ class RegistrationScreen extends StatefulWidget {
 }
 
 class _RegistrationScreenState extends State<RegistrationScreen> {
+  int userType=-1;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         leading: IconButton(
           icon: Icon(Icons.arrow_back),
-          onPressed: (){
+          onPressed: () {
             Navigator.pop(context);
           },
         ),
         title: Text('Register'),
       ),
-    );;
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          const Center(
+            child:  Text(
+              'Select the type of account:',
+              style: TextStyle(
+                fontSize: 24.0
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 16.0, horizontal: 5.0),
+            child: Material(
+              elevation: 5.0,
+              color: Colors.blue,
+              borderRadius: BorderRadius.circular(30.0),
+              child: MaterialButton(
+                child: const Text(
+                  'Donor',
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                ),
+                onPressed: (){
+                  Navigator.pushNamed(context, DonorRegistrationScreen.id);
+                },
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 16.0, horizontal: 5.0),
+            child: Material(
+              elevation: 5.0,
+              color: Colors.blue,
+              borderRadius: BorderRadius.circular(30.0),
+              child: MaterialButton(
+                child: const Text(
+                  'Organization',
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                ),
+                onPressed: (){
+                  Navigator.pushNamed(context, OrganizationRegistrationScreen.id);
+                },
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+    ;
   }
 }
