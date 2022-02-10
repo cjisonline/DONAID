9c7d0df5c41d9218a29d1ce427522a53c309019b
DStawickiDS <79551177+DStawickiDS@users.noreply.github.com>
2022-02-09 19:08:49 -0500

    fixing login and testing js functions

--- login.js
+++ login.js
@@ -1,47 +1,33 @@
-var firebaseConfig = {
-    apiKey: " AIzaSyCXEBNOHoGPQ9VDIaO8BnGZNi9uGy21Wig ",
-    authDomain: "donaid-d3244.firebaseapp.com ",
-    projectId: "donaid-d3244",
-    storageBucket: "donaid-d3244.appspot.com",
-    messagingSenderId: "240146097284",
-    appId: "1:240146097284:android:eafc4f1d40814e961cced1",
-};
+(function(){
+    var ui = new firebaseui.auth.AuthUI(firebase.auth());
 
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-const auth = firebase.auth();
+    var uiConfig = {
+        callbacks: {
+          signInSuccessWithAuthResult: function(authResult, redirectUrl) {
+            // User successfully signed in.
+            // Return type determines whether we continue the redirect automatically
+            // or whether we leave that to developer to handle.
+            return true;
+          },
+          uiShown: function() {
+            // The widget is rendered.
+            // Hide the loader.
+            document.getElementById('loader').style.display = 'none';
+          }
+        },
+        // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
+        signInFlow: 'popup',
+        signInSuccessUrl: 'dashboard.html',
+        signInOptions: [
+          firebase.auth.EmailAuthProvider.PROVIDER_ID
+        ],
+        // Terms of service url.
+        tosUrl: 'dashboard.html',
+        // Privacy policy url.
+        privacyPolicyUrl: '<your-privacy-policy-url>'
+      };
 
-// Login
-function loginAdmin(){
-    var email = document.getElementById("email");
-    var password  = document.getElementById("password");
+      ui.start('#firebaseui-auth-container', uiConfig);
 
-    const promise = auth.signInWithEmailAndPassword(email.value,password.value)
-    .then(function(){
-        console.log("Login Successful");
-        window.location.href = 'dashboard.html'; // Redirect after successful login
-    });
-    promise.catch(e=>alert(e.message));
-}
-
-// Logout
-function logoutAdmin(){
-    auth.signOut()
-    .then(function(){
-        console.log("Logout Successful");
-        window.location = 'index.html'; // Index redirect upon logout
-    });
-    alert("Logged Out");
-}
-
-// Authenticate user information
-firebase.auth().onAuthStateChanged((user)=>{
-    if(user){
-        var email = user.email;
-        alert("User: " + email + " signed in successfully!");
-    }
-    
-    else{
-        alert("User not found. Please try again!")
-    }
-  })
+      
+})()
