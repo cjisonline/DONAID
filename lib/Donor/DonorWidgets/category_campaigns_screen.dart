e1bea486e9e95dfb59903a0ddca5eb4d556cfd91
CJ Fox <55763324+cj-fox@users.noreply.github.com>
2022-02-15 15:59:32 -0500

    Implemented the campaigns body. Charity campaigns title and description is shown along with progress bar.

--- lib/Donor/DonorWidgets/category_campaigns_screen.dart
+++ lib/Donor/DonorWidgets/category_campaigns_screen.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:donaid/Models/Campaign.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 import 'donor_bottom_navigation_bar.dart';
@@ -8,16 +9,23 @@ import 'donor_drawer.dart';
 class CategoryCampaignsScreen extends StatefulWidget {
   static const id = 'category_campaigns_screen';
   final categoryName;
-  const CategoryCampaignsScreen({Key? key, this.categoryName}) : super(key: key);
+  const CategoryCampaignsScreen({Key? key, this.categoryName})
+      : super(key: key);
 
   @override
-  _CategoryCampaignsScreenState createState() => _CategoryCampaignsScreenState();
+  _CategoryCampaignsScreenState createState() =>
+      _CategoryCampaignsScreenState();
 }
 
 class _CategoryCampaignsScreenState extends State<CategoryCampaignsScreen> {
   final _firestore = FirebaseFirestore.instance;
   List<Campaign> campaigns = [];
 
+  @override
+  void initState() {
+    super.initState();
+    _getCampaigns();
+  }
 
   _getCampaigns() async {
     var ret = await _firestore.collection('UrgentCases').get();
@@ -31,11 +39,51 @@ class _CategoryCampaignsScreenState extends State<CategoryCampaignsScreen> {
           endDate: element.data()['endDate'],
           dateCreated: element.data()['dateCreated'],
           id: element.data()['id'],
-          organizationID: element.data()['organizationID']
-      );
+          organizationID: element.data()['organizationID']);
       campaigns.add(campaign);
     }
     setState(() {});
+  }
+
+  _categoryCampaignsBody() {
+    return ListView.builder(
+        itemCount: campaigns.length,
+        shrinkWrap: true,
+        itemBuilder: (context, int index) {
+          //TODO: Create list of charity campaigns here
+          return Card(
+
+            child: Column(
+              children: [
+                ListTile(
+                  onTap: () {
+                    //TODO: implement on tap
+                  },
+                  title: Text(campaigns[index].title),
+                  subtitle: Text(campaigns[index].description),
+                ),
+                Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+                  Text('${(campaigns[index].amountRaised/campaigns[index].goalAmount)*100}%',
+                      textAlign: TextAlign.left,
+                      style: const TextStyle(color: Colors.black, fontSize: 15)),
+                  Text(
+                    '\$${campaigns[index].goalAmount}',
+                    textAlign: TextAlign.start,
+                    style: const TextStyle(color: Colors.black, fontSize: 15),
+                  ),
+                ]),
+                LinearProgressIndicator(
+                  backgroundColor: Colors.grey,
+                  valueColor: AlwaysStoppedAnimation<Color>(
+                      Theme.of(context).primaryColor),
+                  value: (campaigns[index].amountRaised/campaigns[index].goalAmount),
+                  minHeight: 10,
+                ),
+                Divider()
+              ],
+            ),
+          );
+        });
   }
 
   @override
@@ -51,7 +99,7 @@ class _CategoryCampaignsScreenState extends State<CategoryCampaignsScreen> {
         ),
       ),
       drawer: const DonorDrawer(),
-      body: Center(child: Text('Name: ${widget.categoryName}')),
+      body: _categoryCampaignsBody(),
       bottomNavigationBar: const DonorBottomNavigationBar(),
     );
   }
