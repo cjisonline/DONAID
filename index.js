c49c7402addd17e3bbd7ea026d00d944c2075755
CJ Fox <cjfox62600@gmail.com>
2022-02-17 13:08:10 -0500

    changing api key

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 // This is a public sample test API key.
 // Don’t submit any personally identifiable information in requests made with this key.
 // Sign in to see your own test API key embedded in code samples.
-const stripe = require("stripe")('Bearer sk_test_51KTuiGEvfimLlZrspSXbovMmnyU9eJsrzUOSatcAYvz3AfLDE5QcgPOX6oPN6FuzKVhBOETTWiNFWLRVoTm0OURb00HhwsIFwH');
+const stripe = require("stripe")('sk_test_51KTuiGEvfimLlZrspSXbovMmnyU9eJsrzUOSatcAYvz3AfLDE5QcgPOX6oPN6FuzKVhBOETTWiNFWLRVoTm0OURb00HhwsIFwH');
 
 app.use(express.static("public"));
 app.use(express.json());
