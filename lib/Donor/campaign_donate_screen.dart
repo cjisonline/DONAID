fd4ed89131e2fe91f6d8af499eacc8175c7d726e
CJ Fox <55763324+cj-fox@users.noreply.github.com>
2022-02-17 12:17:00 -0500

    Implemented the donate functionality. Donating with stripe allows donating with decimals too. Pop up payment sheet displayed when user clicks donate button.

--- lib/Donor/campaign_donate_screen.dart
+++ lib/Donor/campaign_donate_screen.dart
@@ -1,9 +1,12 @@
+import 'dart:convert';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:donaid/Models/Campaign.dart';
 import 'package:flutter/material.dart';
 import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
-
+import 'package:flutter_stripe/flutter_stripe.dart';
 import 'DonorWidgets/donor_bottom_navigation_bar.dart';
 import 'DonorWidgets/donor_drawer.dart';
+import 'package:http/http.dart' as http;
 
 class CampaignDonateScreen extends StatefulWidget {
   Campaign campaign;
@@ -15,6 +18,8 @@ class CampaignDonateScreen extends StatefulWidget {
 
 class _CampaignDonateScreenState extends State<CampaignDonateScreen> {
   final _formKey = GlobalKey<FormState>();
+  Map<String, dynamic>? paymentIntentData;
+  String donationAmount = "";
   bool showLoadingSpinner = false;
 
   _campaignDonateBody() {
@@ -63,10 +68,12 @@ class _CampaignDonateScreenState extends State<CampaignDonateScreen> {
                       padding: const EdgeInsets.all(8.0),
                       child: TextFormField(
                         onChanged: (value) {
-                          //TODO: Implement onChanged
+                          donationAmount = value.toString();
                         },
                         validator: (value) {
-                          //TODO: Implement validator
+                          if (value!.isEmpty) {
+                            return 'Please enter a valid payment amount.';
+                          }
                         },
                         keyboardType: const TextInputType.numberWithOptions(
                             decimal: true),
@@ -105,6 +112,11 @@ class _CampaignDonateScreenState extends State<CampaignDonateScreen> {
                                 showLoadingSpinner = true;
                               });
                               //TODO:Implement Donate function
+                              await makePayment();
+
+                              setState(() {
+                                showLoadingSpinner=false;
+                              });
                             }
                           },
                         ),
@@ -116,6 +128,83 @@ class _CampaignDonateScreenState extends State<CampaignDonateScreen> {
         ]),
       )),
     );
+  }
+
+  Future<void> makePayment() async {
+    try {
+      paymentIntentData =
+          await createPaymentIntent(donationAmount, 'USD');
+      await Stripe.instance.initPaymentSheet(
+          paymentSheetParameters: SetupPaymentSheetParameters(
+        paymentIntentClientSecret: paymentIntentData!['client_secret'],
+        applePay: true,
+        googlePay: true,
+        style: ThemeMode.dark,
+        merchantCountryCode: 'US',
+        merchantDisplayName: 'DONAID',
+      ));
+
+      print(paymentIntentData);
+      displayPaymentSheet();
+    } catch (e) {
+      print('Excpetion: ${e.toString()}');
+    }
+  }
+
+  displayPaymentSheet() async {
+    try {
+      Stripe.instance.presentPaymentSheet(
+        parameters: PresentPaymentSheetParameters(
+            clientSecret: paymentIntentData!['client_secret'],
+          confirmPayment: true,
+        ),
+
+      );
+
+      setState(() {
+        paymentIntentData = null;
+      });
+
+      ScaffoldMessenger.of(context)
+          .showSnackBar(const SnackBar(content: Text('Paid successfully!')));
+    } on StripeException catch (e) {
+      print('Stripe Exception: ${e.toString()}');
+
+      showDialog(
+        context: context,
+        builder: (_) => const AlertDialog(
+          content: Text('Payment Cancelled.'),
+        ),
+      );
+    }
+  }
+
+  createPaymentIntent(String amount, String currency) async {
+    try {
+      Map<String, dynamic> body = {
+        'amount': calculateAmount(amount),
+        'currency': currency,
+        'payment_method_types[]': 'card'
+      };
+
+      var response = await http.post(
+          Uri.parse('https://api.stripe.com/v1/payment_intents'),
+          body: body,
+          headers: {
+            'Authorization':
+                'Bearer sk_test_51KTuiGEvfimLlZrspSXbovMmnyU9eJsrzUOSatcAYvz3AfLDE5QcgPOX6oPN6FuzKVhBOETTWiNFWLRVoTm0OURb00HhwsIFwH',
+            'Content-Type': 'application/x-www-form-urlencoded'
+          });
+
+      return jsonDecode(response.body.toString());
+    } catch (e) {
+      print('Exception: ${e.toString()}');
+    }
+  }
+
+  calculateAmount(String amount) {
+    final price = (double.parse(amount)*100).toInt();
+    return price.toString();
   }
 
   @override
