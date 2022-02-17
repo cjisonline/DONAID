c4b1ca2f25f252e7ee32fca7c1353f6015cba98f
CJ Fox <cjfox62600@gmail.com>
2022-02-17 13:37:04 -0500

    hardcoded for testing

--- index.js
+++ index.js
@@ -15,9 +15,9 @@ app.post("/create-payment-intent", async (req, res) => {
 
   // Create a PaymentIntent with the order amount and currency
   const paymentIntent = await stripe.paymentIntents.create({
-    amount: body['amount'],
-    currency: body['currency'],
-    payment_method_types: body['payment_method_types[]']
+    amount: 200,
+    currency: 'usd',
+    payment_method_types: card
   });
 
   res.send({
