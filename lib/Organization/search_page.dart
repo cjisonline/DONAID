a772124c6b4feab57f1a9ea47921836ae5d0c068
Raisa Zaman <gn8271@wayne.edu>
2022-02-26 11:08:29 -0500

    Fixed the goal formatting and added an on tap for the list tiles for being able to navigate to the extended view in the future.

--- lib/Organization/search_page.dart
+++ lib/Organization/search_page.dart
@@ -5,6 +5,7 @@ import 'package:donaid/Models/Campaign.dart';
 import 'package:donaid/Models/UrgentCase.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 import 'OrganizationWidget/button_nav_bar.dart';
 
@@ -27,6 +28,7 @@ class _OrgSearchPageState extends State<OrgSearchPage> {
   List<UrgentCase> urgentCases = [];
   List<Map<String, dynamic>> _foundUsers = [];
   final List<Map<String, dynamic>> _allUsers = [];
+  var f = NumberFormat("###,###.0#", "en_US");
 
   void _getCurrentUser() {
     loggedInUser = _auth.currentUser;
@@ -39,7 +41,6 @@ class _OrgSearchPageState extends State<OrgSearchPage> {
     _foundUsers = _allUsers;
     super.initState();
   }
-
 
   _getCampaign() async {
     var ret = await _firestore
@@ -106,7 +107,7 @@ class _OrgSearchPageState extends State<OrgSearchPage> {
           dateCreated: element.data()['dateCreated'],
           id: element.data()['id'],
           organizationID:
-          element.data()['organizationID']); // need to add category
+              element.data()['organizationID']); // need to add category
       beneficiaries.add(beneficiary);
       print(beneficiary.name);
     }
@@ -114,45 +115,46 @@ class _OrgSearchPageState extends State<OrgSearchPage> {
     _getAllData();
   }
 
-  _getAllData(){
-    for(var i=0; i < urgentCases.length; i++){
-      _allUsers.add( {"name": urgentCases[i].title, "goal":
-      urgentCases[i].goalAmount.toString(), "endDate":
-      urgentCases[i].endDate.toDate().
-      toString().substring(0, urgentCases[i].
-      endDate.toDate().toString().indexOf(' '))});
+  _getAllData() {
+    for (var i = 0; i < urgentCases.length; i++) {
+      _allUsers.add({
+        "name": urgentCases[i].title,
+        "goal": f.format(urgentCases[i].goalAmount).toString(),
+        "endDate": urgentCases[i].endDate.toDate().toString().substring(
+            0, urgentCases[i].endDate.toDate().toString().indexOf(' '))
+      });
     }
-    for(var i=0; i < campaigns.length; i++){
-      _allUsers.add( {"name": campaigns[i].title, "goal":
-      campaigns[i].goalAmount.toString(), "endDate":
-      campaigns[i].endDate.toDate().
-      toString().substring(0, campaigns[i].
-      endDate.toDate().toString().indexOf(' '))});
+    for (var i = 0; i < campaigns.length; i++) {
+      _allUsers.add({
+        "name": campaigns[i].title,
+        "goal": f.format(campaigns[i].goalAmount).toString(),
+        "endDate": campaigns[i]
+            .endDate
+            .toDate()
+            .toString()
+            .substring(0, campaigns[i].endDate.toDate().toString().indexOf(' '))
+      });
     }
-    for(var i=0; i < beneficiaries.length; i++){
-      _allUsers.add( {"name": beneficiaries[i].name, "goal":
-      beneficiaries[i].goalAmount.toString(), "endDate":
-      beneficiaries[i].endDate.toDate().
-      toString().substring(0, beneficiaries[i].
-      endDate.toDate().toString().indexOf(' '))});
+    for (var i = 0; i < beneficiaries.length; i++) {
+      _allUsers.add({
+        "name": beneficiaries[i].name,
+        "goal": f.format(beneficiaries[i].goalAmount).toString(),
+        "endDate": beneficiaries[i].endDate.toDate().toString().substring(
+            0, beneficiaries[i].endDate.toDate().toString().indexOf(' '))
+      });
     }
-    print ("Length: " + _allUsers.length.toString());
+    print("Length: " + _allUsers.length.toString());
   }
 
-
-
-  // This function is called whenever the text field changes
-  void _runFilter(String enteredKeyword) {
+  void _searchResults(String enteredKeyword) {
     List<Map<String, dynamic>> results = [];
     if (enteredKeyword.isEmpty) {
-      // if the search field is empty or only contains white-space, we'll display all users
       results = _allUsers;
     } else {
       results = _allUsers
           .where((user) =>
-          user["name"].toLowerCase().contains(enteredKeyword.toLowerCase()))
+              user["name"].toLowerCase().contains(enteredKeyword.toLowerCase()))
           .toList();
-      // we use the toLowerCase() method to make it case-insensitive
     }
 
     // Refresh the UI
@@ -164,49 +166,58 @@ class _OrgSearchPageState extends State<OrgSearchPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('DONAID'),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(10),
-        child: Column(
-          children: [
-            const SizedBox(
-              height: 20,
-            ),
-            TextField(
-              onChanged: (value) => _runFilter(value),
-              decoration: const InputDecoration(
-                  labelText: 'Search', suffixIcon: Icon(Icons.search)),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            Expanded(
-              child: _foundUsers.isNotEmpty
-                  ? ListView.builder(
-                itemCount: _foundUsers.length,
-                itemBuilder: (context, index) => Card(
-                  key: ValueKey(_foundUsers[index]["name"]),
-                  color: Colors.white,
-                  elevation: 4,
-                  margin: const EdgeInsets.symmetric(vertical: 10),
-                  child: ListTile(
-                    title: Text(_foundUsers[index]["name"].toString(),textScaleFactor: 1.5,),
-                      subtitle: Text(_foundUsers[index]['goal']),
-                      trailing: Text(_foundUsers[index]["endDate"].toString()),
-                  ),
-                ),
-              )
-                  : const Text(
-                'No results found',
-                style: TextStyle(fontSize: 24),
-              ),
-            ),
-          ],
+        appBar: AppBar(
+          title: const Text('DONAID'),
         ),
-      ),
-      bottomNavigationBar: ButtomNavigation()
-    );
+        body: Padding(
+          padding: const EdgeInsets.all(10),
+          child: Column(
+            children: [
+              const SizedBox(
+                height: 20,
+              ),
+              TextField(
+                onChanged: (value) => _searchResults(value),
+                decoration: const InputDecoration(
+                    labelText: 'Search', suffixIcon: Icon(Icons.search)),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Expanded(
+                child: _foundUsers.isNotEmpty
+                    ? ListView.builder(
+                        itemCount: _foundUsers.length,
+                        itemBuilder: (context, index) => Card(
+                          key: ValueKey(_foundUsers[index]["name"]),
+                          color: Colors.white,
+                          elevation: 4,
+                          margin: const EdgeInsets.symmetric(vertical: 10),
+                          child: ListTile(
+                            title: Text(
+                              _foundUsers[index]["name"].toString(),
+                              textScaleFactor: 1.5,
+                            ),
+                            subtitle:
+                                Text("\u0024 " + _foundUsers[index]['goal']),
+                            trailing:
+                                Text(_foundUsers[index]["endDate"].toString()),
+                            onTap: () {
+                              setState(() {
+                                //Add the extended view page here
+                              });
+                            },
+                          ),
+                        ),
+                      )
+                    : const Text(
+                        'No results found',
+                        style: TextStyle(fontSize: 24),
+                      ),
+              ),
+            ],
+          ),
+        ),
+        bottomNavigationBar: ButtomNavigation());
   }
 }
