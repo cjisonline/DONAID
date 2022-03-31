0fae83136a53bfcd09e0cdad45c8a28e8cf66510
CJ Fox <55763324+cj-fox@users.noreply.github.com>
2022-03-31 16:15:29 -0400

    Implemented org search UI change per prototype 3 feedback

--- lib/Organization/search_page.dart
+++ lib/Organization/search_page.dart
@@ -365,43 +365,41 @@ class _OrgSearchPageState extends State<OrgSearchPage> {
           const SizedBox(
             height: 20,
           ),
-          Row(
-            children: <Widget>[
-              Expanded(
-                child: DropdownButtonFormField <String>(
-                  decoration: InputDecoration(
-                      label: Center(
-                          child: Text('category'.tr)
-                      ),
-                      border: const OutlineInputBorder(
-                        borderRadius:
-                        BorderRadius.all(Radius.circular(12.0)),
-                      )),
-                  icon:  Visibility(visible: false, child: Icon(Icons.keyboard_arrow_down)),
-                  value: categoryFilterController.text.isNotEmpty ? categoryFilterController.text : null,
-                  items: campaignCategory == null? []: campaignCategory.map((items) {
-                    return DropdownMenuItem<String>(
-                      child: Text(
-                        items,
-                      ),
-                      value: items,
-                    );
-                  }).toList(),
-                  onChanged: (val) => setState(() {
-                    categoryFilterController.text = val.toString();
-                    _filterResults();
-                  }),
+          Container(
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: DropdownButtonFormField <String>(
+                    decoration: InputDecoration(
+                        label: Center(
+                            child: Text('category'.tr)
+                        ),
+                        border: const OutlineInputBorder(
+                          borderRadius:
+                          BorderRadius.all(Radius.circular(12.0)),
+                        )),
+                    icon:  Visibility(visible: false, child: Icon(Icons.keyboard_arrow_down)),
+                    value: categoryFilterController.text.isNotEmpty ? categoryFilterController.text : null,
+                    items: campaignCategory == null? []: campaignCategory.map((items) {
+                      return DropdownMenuItem<String>(
+                        child: Text(
+                          items,
+                        ),
+                        value: items,
+                      );
+                    }).toList(),
+                    onChanged: (val) => setState(() {
+                      categoryFilterController.text = val.toString();
+                      _filterResults();
+                    }),
+                  ),
                 ),
-              ),
-            ],
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-          Row(
-            children: [
-              Expanded(
+                SizedBox(
+                  width: 20,
+                ),
+                Expanded(
                   child: DropdownButtonFormField<String>(
+                    isExpanded: true,
                     decoration: InputDecoration(
                         label: Center(
                             child: Text('Sort By'.tr)
@@ -422,8 +420,13 @@ class _OrgSearchPageState extends State<OrgSearchPage> {
                       sortingController.text = val.toString();
                       _sortResults();
                     }),
-                  )),
-            ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          const SizedBox(
+            height: 20,
           ),
 
           Expanded(
@@ -504,15 +507,10 @@ class _OrgSearchPageState extends State<OrgSearchPage> {
                   }),
                 ),
               ),
-            ],
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-          Row(
-            children: [
+              SizedBox(width: 20,),
               Expanded(
                   child: DropdownButtonFormField<String>(
+                    isExpanded: true,
                     decoration: InputDecoration(
                         label: Center(
                             child: Text('Sort By'.tr)
@@ -535,6 +533,9 @@ class _OrgSearchPageState extends State<OrgSearchPage> {
                     }),
                   )),
             ],
+          ),
+          const SizedBox(
+            height: 20,
           ),
 
           Expanded(
@@ -615,15 +616,10 @@ class _OrgSearchPageState extends State<OrgSearchPage> {
                   }),
                 ),
               ),
-            ],
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-          Row(
-            children: [
+              SizedBox(width: 20,),
               Expanded(
                   child: DropdownButtonFormField<String>(
+                    isExpanded: true,
                     decoration: InputDecoration(
                         label: Center(
                             child: Text('Sort By'.tr)
@@ -646,6 +642,9 @@ class _OrgSearchPageState extends State<OrgSearchPage> {
                     }),
                   )),
             ],
+          ),
+          const SizedBox(
+            height: 20,
           ),
 
           Expanded(
