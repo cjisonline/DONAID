4663a55d81f2ef1f19643367b1d0d47fb9355af6
CJ Fox <55763324+cj-fox@users.noreply.github.com>
2022-04-13 16:57:42 -0400

    Made dashboard cards consistent in the organization UI

--- lib/Organization/OrganizationWidget/urgent_case_card.dart
+++ lib/Organization/OrganizationWidget/urgent_case_card.dart
@@ -48,12 +48,15 @@ class _UrgentCaseCardState extends State<UrgentCaseCard> {
               // Display Urgent Case title populated from firebase
               Padding(
                 padding: const EdgeInsets.all(20.0),
-                child: Text(widget.urgentCase.title,
-                    textAlign: TextAlign.center,
-                    style: const TextStyle(
-                      color: Colors.black,
-                      fontSize: 20,
-                    )),
+                child: Container(
+                  height: 50,
+                  child: Text(widget.urgentCase.title,
+                      textAlign: TextAlign.center,
+                      style: const TextStyle(
+                        color: Colors.black,
+                        fontSize: 20,
+                      )),
+                ),
               ),
               // Display Urgent case description populated from firebase
               SizedBox(
